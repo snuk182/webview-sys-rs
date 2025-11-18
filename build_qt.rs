966b703117e3fdff2539fad5987b524ecda2568b
Serhii Plyhun <S.Plyhun@gentics.com>
2025-11-18 17:13:09 +0100

    Qt works

--- build_qt.rs
+++ build_qt.rs
@@ -6,6 +6,7 @@ pub fn main() {
     config.define("WEBVIEW_BUILD_STATIC_LIBRARY", "true");
     config.define("WEBVIEW_IS_TOP_LEVEL_BUILD", "false");
     config.define("WEBVIEW_QT", "true");
+    config.define("CMAKE_CXX_STANDARD", "11");
 
     if cfg!(debug_assertions) {
         config.define("CMAKE_BUILD_TYPE", "Debug");
@@ -16,6 +17,11 @@ pub fn main() {
 
     let mut config = cmake::Config::new("sys/webview");
     config.no_build_target(true);
-    config.no_build_target(true).no_build_target(true);
-    config.build();
+    let dst = config.build();
+
+    println!("cargo:rustc-link-search=native={}/build/core", dst.display());
+    println!("cargo:rustc-link-lib=static=webviewd");
+    println!("cargo:rustc-link-lib=dylib=Qt5WebEngineWidgets");
+    println!("cargo:rustc-link-lib=dylib=Qt5WebChannel");
+    println!("cargo:rustc-link-lib=dylib=stdc++");
 }
